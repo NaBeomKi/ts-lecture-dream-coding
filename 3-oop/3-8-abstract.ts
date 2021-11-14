9e53af2931e62edaf00a1655c723949c7886871e
nabeomki <beomki69@gmail.com>
2021-11-14 21:33:11 +0900

    Abstract

---
+++ 3-oop/3-8-abstract.ts
@@ -0,0 +1,96 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  abstract class CoffeeMachine implements CoffeeMaker {
+    // abstract class는 인스턴스를 생성할 수 없음
+    private static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      // private는 상속할 수 없으므로 public 또는 자식에서 접근가능한 protected 으로
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBeans(beans: number): void {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean(): void {
+      console.log("Cleaning the machine...🧼");
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up...🔥");
+    }
+
+    protected abstract extract(shots: number): CoffeeCup;
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+
+    private steamMilk(): void {
+      console.log("Steaming some milk...🥛");
+    }
+
+    protected extract(shots: number): CoffeeCup {
+      this.steamMilk();
+      return {
+        shots,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    protected extract(shots: number): CoffeeCup {
+      return {
+        shots,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const sweetMachine = new SweetCoffeeMaker(65);
+  const sweetCoffee = sweetMachine.makeCoffee(2);
+  console.log(sweetMachine, sweetCoffee);
+
+  const machines: CoffeeMaker[] = [
+    new CaffeLatteMachine(20, "1"),
+    new SweetCoffeeMaker(16),
+    new CaffeLatteMachine(20, "3"),
+    new SweetCoffeeMaker(16),
+  ];
+
+  machines.forEach((machine) => {
+    console.log("-----------------------------");
+    machine.makeCoffee(1);
+  });
+}
