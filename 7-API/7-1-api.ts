694d3ba91aad62c88f5b4024023e57d7c5588d52
nabeomki <beomki69@gmail.com>
2021-11-17 23:05:05 +0900

    API

---
+++ 7-API/7-1-api.ts
@@ -0,0 +1,34 @@
+// https://github.com/microsoft/TypeScript/blob/master/lib/lib.es5.d.ts
+Array;
+[1, 2].map;
+
+type Student = {
+  passed: boolean;
+};
+
+const students: Student[] = [
+  { passed: true },
+  { passed: true },
+  { passed: true },
+];
+
+const result = students.every((student) => student.passed);
+console.log(result);
+
+class Animal {}
+class Cat extends Animal {
+  isCat: boolean = true;
+  // isCat: string = "nyan";
+}
+class Dog extends Animal {
+  isDog: boolean = false;
+  // isDog: string = "wow";
+}
+
+const animals: Animal[] = [new Cat(), new Cat(), new Dog()];
+
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isCat !== undefined;
+}
+
+console.log(animals.every<Cat>(isCat));
