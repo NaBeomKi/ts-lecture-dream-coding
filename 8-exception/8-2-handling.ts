038e527162459fa235e90057b70056de0fcea9ac
nabeomki <beomki69@gmail.com>
2021-12-02 23:09:27 +0900

    Error State

--- 8-exception/8-2-handling.ts
+++ 8-exception/8-2-handling.ts
@@ -1,7 +1,11 @@
 {
+  class TimeoutError extends Error {}
+
+  class OfflineError extends Error {}
+
   class NetworkClient {
     tryConnect(): void {
-      throw new Error("no network!");
+      throw new OfflineError("no network!");
     }
   }
 
@@ -21,6 +25,10 @@
         this.userService.login();
       } catch (error) {
         // Show dialog to user
+        if (error instanceof OfflineError) {
+          // tsconfig에서 target을 es6이상으로 하면 가능하지만,
+          // 그 이전 버전의 경우 TypeScript에서 구현된 catch()에는 어떠한 타입정보도 전달되지 않아서 instanceOf를 사용할 수 없음
+        }
       }
       // 에러가 발생했을때 에러를 처리할 수 있는 곳에서 try-catch를 하는 것이 낫다.
     }
