3c4f4ab713a86b81bca2a040db2cf2e381e1de01
nabeomki <beomki69@gmail.com>
2021-12-06 00:49:10 +0900

    Advanced - partial

---
+++ 9-advanced/9-6-partial.ts
@@ -0,0 +1,25 @@
+{
+  type ToDo = {
+    title: string;
+    description: string;
+    label: string;
+    priority: "high" | "low";
+  };
+
+  function updateTodo(todo: ToDo, fieldsToUpdate: Partial<ToDo>): ToDo {
+    return {
+      ...todo,
+      ...fieldsToUpdate,
+    };
+  }
+
+  const todo: ToDo = {
+    title: "learn TS",
+    description: "study hard",
+    label: "study",
+    priority: "high",
+  };
+
+  const updatedTodo = updateTodo(todo, { priority: "low" });
+  console.log(updatedTodo);
+}
